28a49ad4ddb8f7b5dc159223302b3bbb6bc22f44
schoettker <lennartschoettker@hotmail.com>
2016-12-14 23:33:34 +0100

    add buttons to increase and decrease the related duration (pause, session)

--- script.js
+++ script.js
@@ -52,14 +52,28 @@ var elSessionLength = document.getElementById('session-length');
 
 // clock.setTime(elSessionLength.innerText*60);
 // clock.start();
-function changeLength(buttonId) {
-  var element = document.getElementById(buttonId),
-    length = element.parentElement.querySelector('.length');
-  element.addEventListener('click', function(e) {
-    length.innerText = Number(length.innerText) + 1;
+function buttonsIncreaseAndDecreaseLengths() {
+  function decrease(buttonId) {
+    var element = document.getElementById(buttonId),
+      length = element.parentElement.querySelector('.length');
+    element.addEventListener('click', function(e) {
+      length.innerText = Number(length.innerText) - 1;
     });
+  }
+  function increase(buttonId) {
+    var element = document.getElementById(buttonId),
+      length = element.parentElement.querySelector('.length');
+    element.addEventListener('click', function(e) {
+      length.innerText = Number(length.innerText) + 1;
+    });
+  }
+
+  increase('inc-session');
+  decrease('dec-session');
+  increase('inc-pause');
+  decrease('dec-pause');
 }
-changeLength('test');
+buttonsIncreaseAndDecreaseLengths();
 // // changeLengths('session-
 // var but = document.getElementById('inc-session').parentElement;
 // console.log(but.querySelector('.session-length'));
